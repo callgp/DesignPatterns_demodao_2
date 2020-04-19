7d34126960717388a4ef352a2c0e17133b731068
callgp <callgp@gmail.com>
2020-04-19 16:47:53 -0400

    04192020 initial add

--- src/main/java/com/gp/patterns/dao/EmployeeDAOImpl.java
+++ src/main/java/com/gp/patterns/dao/EmployeeDAOImpl.java
@@ -16,6 +16,7 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
 		String sql="insert into employee values(?,?)";
 		jdbcTemplate.update(sql,employee.getId(),employee.getName());
+		System.out.println("testing git 04182020");
 	}
 
 }
